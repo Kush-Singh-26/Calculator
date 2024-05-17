f10779666d1588ca8e906ab52d4e5177039ceb4d
Kush-Singh-26 <kushsingh2004+webdev@gmail.com>
2024-05-17 11:58:33 +0530

    final working of calculator complete

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let value2 = '';
 let curr_input;
 let operator = '';
 let operator_value = '';
+let round = '';
 
 const input_display = document.querySelector(".display-user-input");
 const output_display = document.querySelector(".display-user-output");
@@ -42,19 +43,25 @@ const controlButton = document.querySelectorAll(".control");
 const operatorButton = document.querySelectorAll(".operator");
 const equalButton = document.querySelectorAll(".equal");
 const decimalButton = document.querySelectorAll(".decimal");
+const signButton = document.querySelectorAll(".sign");
+
 
 function handleClick(button) {
     curr_input = button.className;
 
     if (curr_input.includes("number") || curr_input.includes("decimal")) {
         if (operator === '') {
-            value1 += button.textContent;
-            display(value1);
+            if (!(button.textContent === '.' && value1.includes('.'))) {
+                value1 += button.textContent;
+                display(value1);
+            }
         } else {
-            value2 += button.textContent;
-            display(value2);
+            if (!(button.textContent === '.' && value2.includes('.'))) {
+                value2 += button.textContent;
+                display(value2);
+            }
         }
-    } else if (curr_input.includes("operator")) {
+    }else if (curr_input.includes("operator")) {
         if (value1 !== '') {
             operator = button.value;
             operator_value = button.textContent;
@@ -70,10 +77,18 @@ function handleClick(button) {
         } else if (button.id === "del") {
             backspace();
         }
-    }
+    } else if (curr_input.includes("sign")) {
+        if(value1 !== '' && operator === ''){
+            value1 = parseFloat((value1) * -1).toString();
+            display(value1);}
+        else if(value2 !== ''){
+            value2 = parseFloat((value2) * -1).toString();
+            display(value2);}
+    }   
+
 }
 
-[numberButton, controlButton, operatorButton, equalButton, decimalButton].forEach(buttons => {
+[numberButton, controlButton, operatorButton, equalButton, decimalButton, signButton].forEach(buttons => {
     buttons.forEach(button => {
         button.addEventListener('click', function () {
             handleClick(button);
@@ -81,8 +96,24 @@ function handleClick(button) {
     });
 });
 
+function maxValue(){
+    if ((value1).toString().length > 9)  {
+        if(round === ''){
+        input_display.textContent = "ERROR"
+        output_display.textContent = "";
+        }
+        
+    }
+
+    else if(value2.length > 9) {
+        input_display.textContent = "ERROR";
+        output_display.textContent = "";
+    }
+}
+
 function displayNumber(num) {
     input_display.textContent = num;
+    maxValue();
 }
 
 function display(value) {
@@ -100,6 +131,7 @@ function clearAll() {
     operator = '';
     displayNumber('');
     output_display.textContent = '';
+    round = '';
 }
 
 function backspace() {
@@ -148,5 +180,16 @@ function operate() {
     value1 = ans.toString();
     value2 = '';
     operator = '';
-    displayNumber(ans);
+    round = "new";
+
+    if(ans.toString().length > 9){
+        displayModifiedResult(ans);
+    }
+    else
+        displayNumber(ans);
 }
+
+function displayModifiedResult(num){
+
+        input_display.textContent = parseFloat(num).toExponential(2);
+    }
